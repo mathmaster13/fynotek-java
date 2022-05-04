35253f2bd30fcf05069da7778d16013f90d99483
mathmaster13 <>
2022-05-03 20:51:12 -0400

    basic suffixes work now, but still one content root

--- demos/MorphemeAnalyzerDemo.kt
+++ demos/MorphemeAnalyzerDemo.kt
@@ -1,6 +1,8 @@
 @file:JvmName("MorphemeAnalyzerDemo")
 
-// TODO numbers (derogatory)
+// TODO thinky thonk. should you make some of these functions part of the main API?
+// TODO numbers and ñojera (derogatory)
+// TODO to verb or not to verb
 
 import com.mathmaster13.fynotek.BaseFynotekWord.Ablaut
 import com.mathmaster13.fynotek.FynotekWord
@@ -51,18 +53,22 @@ fun main() {
         println("This is not a valid fynotek word. Please try again.")
         return
     }
+    if (standaloneWords.contains(word)) {
+        println(word)
+        return
+    }
     for (analysis in singleRootAnalysis(word)) println(analysis)
 }
 
 // Convenience functions
-fun prompt(message: String): String {
+private fun prompt(message: String): String {
     print(message)
     return input.next()
 }
 
 /** Assuming that a word is a single word with no affixes (but it can have ablaut), give the possible dictionary matches */
-fun singleRootAnalysis(word: String): List<String> {
-    if (standaloneWords.contains(word) || ablautValidRoots.contains(word)) return listOf(word)
+private fun singleRootAblautAnalysis(word: String): List<String> {
+    if (ablautValidRoots.contains(word)) return listOf(word)
     val separatedWord = separateVowels(word)
     val pattern = Regex("^" + separatedWord[0] + "[aeiouy]{1,2}" + separatedWord[2] + "$")
     val output = mutableListOf<String>()
@@ -83,4 +89,31 @@ fun singleRootAnalysis(word: String): List<String> {
         }
     }
     return output
+}
+
+/** A single-root analysis that accounts for suffixes, but not prefixes. */
+private fun singleRootAnalysis(word: String): List<String> {
+    val output = mutableListOf<String>()
+    output.addAll(singleRootAblautAnalysis(word)) // Check if any analysis works with no suffix
+    for (suffix in arrayOf("ak", "akñy", "ñy", "ñya", "ñyo")) {
+        if (!word.contains(Regex("$suffix$"))) continue
+        var potentialRoot = word.substring(0, word.length - suffix.length)
+         // Check if any analysis works assuming no filler letters
+        singleRootAblautAnalysis(potentialRoot).forEach { output.add("$it + ${suffixToString(suffix)}") }
+
+        // Check if filler letters are valid
+        // Note: Fynotek shouldn't have any one-letter content roots, but if it does, add (potentialRoot.length <= 1) to the below condition.
+        if (!potentialRoot.contains(Regex("[an]$"))) continue
+        potentialRoot = potentialRoot.substring(0, potentialRoot.length - 1)
+        if (isValidSequence(potentialRoot + suffix)) continue // If filler letters aren't necessary, they won't be used.
+        singleRootAblautAnalysis(potentialRoot).forEach { output.add("$it + ${suffixToString(suffix)}") }
+    }
+    return output
+}
+
+private fun suffixToString(suffix: String) = when (suffix) {
+    "akñy" -> "ak + ñy"
+    "ñya" -> "ñy + a"
+    "ñyo" -> "ñy + o"
+    else -> suffix
 }
