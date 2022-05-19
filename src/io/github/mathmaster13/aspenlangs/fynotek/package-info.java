9083fb651030cbb12b5e299592144c8db358939b
mathmaster13 <>
2022-05-19 19:55:38 -0400

    sigh. one. single. space.

--- src/io/github/mathmaster13/aspenlangs/fynotek/package-info.java
+++ src/io/github/mathmaster13/aspenlangs/fynotek/package-info.java
@@ -2,7 +2,7 @@
  * Provides classes for working with words in Fynotek. More information on Fynotek can be found <a href="https://aspenlangs.neocities.org/fyn.html">here</a>.
  * This project's source code can be found on <a href="https://github.com/mathmaster13/fynotek-java">GitHub</a>.
  * <br><br>
- * All fields, return values, method parameters, etc. in this package are asserted not to be {@code null}unless explicitly annotated with {@code @Nullable}.
+ * All fields, return values, method parameters, etc. in this package are asserted not to be {@code null} unless explicitly annotated with {@code @Nullable}.
  * Kotlin respects these assertions.
  * <br><br>
  * This project is still in development, and features may be changed or removed at any time.
